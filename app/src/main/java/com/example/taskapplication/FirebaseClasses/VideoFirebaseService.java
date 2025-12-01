398c35ff609d54700887f101a2a63812fe252a38
payal01Gupta <payal.newspark20@gmail.com>
2025-12-01 10:06:12 +0530

    date- 1 Dec 2025 message- Video downlaoding using data payload

---
+++ app/src/main/java/com/example/taskapplication/FirebaseClasses/VideoFirebaseService.java
@@ -0,0 +1,34 @@
+package com.example.taskapplication.FirebaseClasses;
+
+import androidx.annotation.NonNull;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import com.example.taskapplication.worker.VideoDownloadWorker;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class VideoFirebaseService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+
+        if (remoteMessage.getData().size() > 0) {
+
+            String videoName = remoteMessage.getData().get("videoname");
+
+            Data data = new Data.Builder()
+                    .putString("videoname", videoName)
+                    .build();
+
+            OneTimeWorkRequest request =
+                    new OneTimeWorkRequest.Builder(VideoDownloadWorker.class)
+                            .setInputData(data)
+                            .build();
+
+            WorkManager.getInstance(this).enqueue(request);
+        }
+    }
+
+}
